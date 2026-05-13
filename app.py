dad30b8c2a35b1c1df6400b8ce3690ab5242d988
chechito1987 <140265087+chechito1987@users.noreply.github.com>
2026-05-12 23:29:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = "sergio2026"
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+
 
 @app.route("/", methods=["GET"])
 def verify():
@@ -15,11 +20,46 @@ def verify():
 
     return "Token incorrecto", 403
 
+
 @app.route("/", methods=["POST"])
 def webhook():
     data = request.get_json()
     print(data)
+
+    try:
+        mensaje = data["entry"][0]["changes"][0]["value"]["messages"][0]
+        numero = mensaje["from"]
+
+        enviar_mensaje(
+            numero,
+            "Hola Sergio 👋 ya recibí tu mensaje. Tu asistente está activo 🚀"
+        )
+
+    except Exception as e:
+        print("Error:", e)
+
     return "OK", 200
+
+
+def enviar_mensaje(numero, texto):
+    url = f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {WHATSAPP_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": numero,
+        "type": "text",
+        "text": {
+            "body": texto
+        }
+    }
+
+    requests.post(url, headers=headers, json=payload)
+
 
 @app.route("/health", methods=["GET"])
 def health():
