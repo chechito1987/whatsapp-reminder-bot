543a110fcf00b107a5dc1927b43c16f3c47548eb
chechito1987 <140265087+chechito1987@users.noreply.github.com>
2026-05-12 23:08:39 -0500

    crear webhook

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "sergio2026"
+
+@app.route("/", methods=["GET"])
+def verify():
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return challenge, 200
+
+    return "Token incorrecto", 403
+
+@app.route("/", methods=["POST"])
+def webhook():
+    data = request.get_json()
+    print(data)
+    return "OK", 200
+
+@app.route("/health", methods=["GET"])
+def health():
+    return "Bot activo", 200
