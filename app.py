792cf2d1b8c98c6fabdcee6d66a879adb75cef53
chechito1987 <140265087+chechito1987@users.noreply.github.com>
2026-05-13 00:08:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = "sergio2026"
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
 
 @app.route("/", methods=["GET"])
@@ -23,21 +27,32 @@ def webhook():
 
     try:
         mensaje = data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"]
+        numero = data["entry"][0]["changes"][0]["value"]["messages"][0]["from"]
 
         print("Mensaje recibido:", mensaje)
 
-        if "hola" in mensaje.lower():
-            print("Responder: Hola Sergio 👋")
+        respuesta = "Hola Sergio 👋"
 
         if "recordar" in mensaje.lower():
-            print("Crear recordatorio")
+            respuesta = "Perfecto, dime qué quieres recordar ⏰"
+
+        requests.post(
+            f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages",
+            headers={
+                "Authorization": f"Bearer {WHATSAPP_TOKEN}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "messaging_product": "whatsapp",
+                "to": numero,
+                "type": "text",
+                "text": {"body": respuesta}
+            }
+        )
+
+        print("Mensaje enviado")
 
     except Exception as e:
         print("Error:", e)
 
     return "OK", 200
-
-
-@app.route("/health", methods=["GET"])
-def health():
-    return "Bot activo", 200
